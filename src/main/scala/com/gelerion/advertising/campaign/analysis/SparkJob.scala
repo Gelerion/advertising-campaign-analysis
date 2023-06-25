9645085fe0a0c769ea143d8ae355bfd96f27c94d
Gelerion <denis.shuvalov1@gmail.com>
2023-06-25 13:53:49 +0300

    naive strategy

--- src/main/scala/com/gelerion/advertising/campaign/analysis/SparkJob.scala
+++ src/main/scala/com/gelerion/advertising/campaign/analysis/SparkJob.scala
@@ -15,8 +15,8 @@ trait SparkJob {
     .builder()
     .appName("usage statistics test")
     .master(s"local[${min(max(nCores - 2, 1), 4)}]") //use up to 4 cores
-    .config("spark.network.timeout", "10000001")
-    .config("spark.executor.heartbeatInterval", "10000000")
+    .config("spark.network.timeout", "10000001") //for debugging, don't use in prod setup
+    .config("spark.executor.heartbeatInterval", "10000000") //for debugging, don't use in prod setup
     .config("spark.sql.shuffle.partitions", "8")
     .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
     .getOrCreate()
